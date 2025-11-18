81c66e59d38ee709d18b47d2b83784dbb81c583a
Stephen Smithwick <stephen.smithwick@gmail.com>
2025-11-18 00:21:49 -0700

    More leetcode work

--- python/leetcode/letter_case_permutations.py
+++ python/leetcode/letter_case_permutations.py
@@ -11,6 +11,6 @@ for s, expected in [
     ("a1b2", ["a1b2", "a1B2", "A1b2", "A1B2"]),
     ("3z4", ["3z4"]),
 ]:
-    print(f"\letterCasePermutation({s})")
+    print(f"letterCasePermutation({s})")
     print(f"            = {Solution().letterCasePermutation(s)}")
     print(f"       expect {expected}")
