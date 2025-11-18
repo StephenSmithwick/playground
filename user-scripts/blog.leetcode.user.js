9aa9257bd8e0a3a606328d8053ec677f6b0c0f40
Stephen Smithwick <stephen.smithwick@gmail.com>
2025-11-18 12:18:37 -0700

    Minimal update to allow loading of leetcode problems which are not included in the algorithms category

--- user-scripts/blog.leetcode.user.js
+++ user-scripts/blog.leetcode.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LeetCode Study Plan
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  Decorate Blog leetcode lists with Leetcode userdata
 // @author       stephensmithwick
 // @match        http://127.0.0.1:4000/leetcode/*
@@ -13,43 +13,62 @@
 (function () {
   "use strict";
 
+  function loadProblems(slug) {
+    GM.xmlHttpRequest({
+      method: "GET",
+      url: `https://leetcode.com/api/problems/${slug}/`,
+      onload: function (leet) {
+        const leetData = JSON.parse(leet.response);
+        console.log(leetData);
+
+        const leetProblems = new Map(
+          leetData.stat_status_pairs.map((x) => [
+            x.stat.question__title_slug,
+            x,
+          ]),
+        );
+
+        [...document.querySelectorAll(".leetcode>li")].forEach((li) => {
+          const slug = li.dataset.slug || li.innerHTML;
+          const url = `https://leetcode.com/problems/${slug}`;
+
+          const problem = leetProblems.get(slug);
+          if (problem) {
+            const id = problem.stat?.frontend_question_id;
+            const title = problem.stat?.question__title;
+            const status = problem.status === "ac" ? "✅" : "⬛";
+            const level = problem.difficulty?.level;
+            const difficulty = ["NA", "🟢", "🟡", "🔴"][level ? level : 0];
+            const total_acs = problem.stat?.total_acs;
+            const total_submitted = problem.stat?.total_submitted;
+            const success_rate =
+              total_acs && total_submitted
+                ? (total_acs / total_submitted).toFixed(2)
+                : "";
+
+            li.innerHTML = `
+                        <data class="status">${status} ${difficulty}</data>
+                        <a href="${url}">${id}. ${title} </a>
+                        <data class="stats">
+                            tries: <data>${total_submitted}</data>
+                            rate: <data>${success_rate}</data>
+                        </data>`;
+            li.classList.add("grid");
+          }
+        });
+      },
+    });
+  }
+
   GM.xmlHttpRequest({
     method: "GET",
-    url: "https://leetcode.com/api/problems/algorithms/",
+    url: "https://leetcode.com/problems/api/card-info/",
     onload: function (leet) {
       const leetData = JSON.parse(leet.response);
       console.log(leetData);
-
-      const leetProblems = new Map(
-        leetData.stat_status_pairs.map((x) => [x.stat.question__title_slug, x]),
-      );
-
-      [...document.querySelectorAll(".leetcode>li")].forEach((li) => {
-        const slug = li.dataset.slug || li.innerHTML;
-        const url = `https://leetcode.com/problems/${slug}`;
-
-        const problem = leetProblems.get(slug);
-        const id = problem?.stat?.frontend_question_id;
-        const title = problem?.stat?.question__title;
-        const status = problem?.status === "ac" ? "✅" : "⬛";
-        const level = problem?.difficulty?.level;
-        const difficulty = ["NA", "🟢", "🟡", "🔴"][level ? level : 0];
-        const total_acs = problem?.stat?.total_acs;
-        const total_submitted = problem?.stat?.total_submitted;
-        const success_rate =
-          total_acs && total_submitted
-            ? (total_acs / total_submitted).toFixed(2)
-            : "";
-
-        li.innerHTML = `
-                    <data class="status">${status} ${difficulty}</data>
-                    <a href="${url}">${id}. ${title} </a>
-                    <data class="stats">
-                        tries: <data>${total_submitted}</data>
-                        rate: <data>${success_rate}</data>
-                    </data>`;
-        li.classList.add("grid");
-      });
+      for (let category of leetData.categories[0]) {
+        loadProblems(category.slug);
+      }
     },
   });
 })();
