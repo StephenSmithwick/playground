a2a38d2a7817a141d54407ad4e99dc6d39047c31
Stephen Smithwick <stephen.smithwick@gmail.com>
2026-02-21 00:46:20 -0700

    Adding logging to a log directory

--- js/agentia/source/logs.ts
+++ js/agentia/source/logs.ts
@@ -1,9 +1,12 @@
 import * as fs from 'fs';
 
 const streams = new Map<string, fs.WriteStream>();
+fs.mkdirSync('logs', {recursive: true});
+
 export function log_to_file<T>(filename: string, message: T): T {
 	let log =
-		streams.get(filename) ?? fs.createWriteStream(filename, {flags: 'a'});
+		streams.get(filename) ??
+		fs.createWriteStream('logs/' + filename, {flags: 'a'});
 	if (!streams.has(filename)) streams.set(filename, log);
 
 	log.write(typeof message === 'string' ? message : JSON.stringify(message));
